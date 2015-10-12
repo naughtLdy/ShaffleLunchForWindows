3a557b04fd98e669a2359c5b29c67709e9c81827
naught <sdax05@gmail.com>
2015-10-12 23:42:38 +0900

    テストデータ追加

---
+++ ShuffleLunch/TestData/TestData.js
@@ -0,0 +1,31 @@
+﻿
+{
+	"desks":[
+		{"name":"random", "max":8, "min":6},
+		{"name":"机1", "max":8, "min":6},
+		{"name":"机2", "max":8, "min":6},
+		{"name":"机3", "max":8, "min":6},
+		{"name":"机4", "max":8, "min":6},
+		{"name":"机5", "max":8, "min":6}
+	],
+	"persons":[
+		{"name":"Aさん", "desk":"random", "image":"vaio.png"},
+		{"name":"Bさん", "desk":"random", "image":"vaio.png"},
+		{"name":"Cさん", "desk":"random", "image":"g.png"},
+		{"name":"Dさん", "desk":"random", "image":"g.png"},
+		{"name":"Eさん", "desk":"random", "image":"g.png"},
+		{"name":"Fさん", "desk":"random", "image":"g.png"},
+		{"name":"Gさん", "desk":"random", "image":"g.png"},
+		{"name":"Hさん", "desk":"random", "image":""},
+		{"name":"Iさん", "desk":"random", "image":""},
+		{"name":"Jさん", "desk":"random", "image":""},
+		{"name":"Kさん", "desk":"random", "image":""},
+		{"name":"Lさん", "desk":"random", "image":""},
+		{"name":"Mさん", "desk":"random", "image":""},
+		{"name":"Nさん", "desk":"random", "image":""},
+		{"name":"Oさん", "desk":"random", "image":""},
+		{"name":"Pさん", "desk":"random", "image":""},
+		{"name":"Qさん", "desk":"random", "image":""}
+	]
+}
+
