59b5d1361c5207ce9937e22a26e1cfbf9f911bc2
naught <sdax05@gmail.com>
2015-10-12 19:49:18 +0900

    デリゲーター追加

---
+++ ShuffleLunch/Utils/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace lunch_proto.Utils
+{
+	class DelegateCommand : ICommand
+	{
+		private Action<Object> _action;
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public void Execute(object parameter)
+		{
+			_action(parameter);
+		}
+
+		public DelegateCommand(Action<Object> action)
+		{
+			_action = action;
+		}
+	}
+}
