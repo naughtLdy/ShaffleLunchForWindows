c04a75b57ffa513d14f94657c72807da4fe56b5f
naught <sdax05@gmail.com>
2015-10-12 19:45:21 +0900

    新規作成

---
+++ ShuffleLunch/ViewModels/WindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuffleLunch.ViewModels
+{
+	class WindowViewModel : INotifyPropertyChanged
+	{
+		#region プロパティ変更通知
+
+		// INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+		{
+			if (object.Equals(storage, value)) return false;
+
+			storage = value;
+			this.OnPropertyChanged(propertyName);
+			return true;
+		}
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var eventHandler = this.PropertyChanged;
+			if (eventHandler != null)
+			{
+				eventHandler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		#endregion
+
+		#region Title 変更通知プロパティ
+
+		private string _title;
+
+		public string Title
+		{
+			get { return this._title; }
+			set
+			{
+				SetProperty(ref _title, value);
+			}
+		}
+
+		#endregion
+
+		public WindowViewModel()
+		{
+			Title = "ShuffleLunch";
+		}
+
+	}
+}
