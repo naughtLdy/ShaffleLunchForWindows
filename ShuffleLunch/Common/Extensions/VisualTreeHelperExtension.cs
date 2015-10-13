85cc55305f29907f6b0576da30d1f0a4316f2e9d
yutaka <shigure@refy.net>
2015-10-13 15:45:47 +0900

    add png export utility.

---
+++ ShuffleLunch/Common/Extensions/VisualTreeHelperExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ShuffleLunch.Common.Extensions
+{
+    public static class VisualTreeHelperExtension
+    {
+        public static T FindFirstChild<T>(this DependencyObject d)
+            where T : DependencyObject
+        {
+            // fast-pass
+            if (d is T) return (T)d;
+            var q = new Queue<DependencyObject>();
+            q.Enqueue(d);
+            while (q.Count > 0)
+            {
+                var e = q.Dequeue();
+                var n = VisualTreeHelper.GetChildrenCount(e);
+                for (var i = 0; i < n; ++i)
+                {
+                    var c = VisualTreeHelper.GetChild(e, i);
+                    if (c is T) return (T) c;
+                    q.Enqueue(c);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ ShuffleLunch/Models/PngExporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ShuffleLunch.Models
+{
+    class PngExporter
+    {
+        public static void Export(FrameworkElement element)
+        {
+            var rtb = new RenderTargetBitmap((int)element.ActualWidth, (int)element.ActualHeight, 96, 96, PixelFormats.Rgb24);
+            rtb.Render(element);
+        }
+    }
+}
